8a8094d5c85b61709e7d8736ef9da090f5676a17
M C <M C>
2016-09-10 18:10:53 +1200

    Initial push

---
+++ js/musichandler.js
@@ -0,0 +1,7 @@
+var myClientId = "d4c36b05ad813b4c4a57a9b0b501cc52";
+function initSC() {
+    // init SoundCloud
+    SC.initialize({
+        client_id: myClientId
+    });
+}
---
+++ js/musichandler.ts
@@ -0,0 +1,8 @@
+var myClientId = "d4c36b05ad813b4c4a57a9b0b501cc52";
+
+function initSC() : void {
+    // init SoundCloud
+    SC.initialize({
+        client_id: myClientId
+    });
+}
